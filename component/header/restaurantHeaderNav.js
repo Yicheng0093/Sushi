c9e2788ab2bcdfc0b34ddbb6bfd06972431452c4
Hung-Cheng-Xi <11146062@ntub.edu.tw>
2024-11-28 23:27:20 +0800

    (feat): 更改元素載入時間

--- component/header/restaurantHeaderNav.js
+++ component/header/restaurantHeaderNav.js
@@ -1,24 +1,41 @@
 class RestaurantHeaderNav extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
+  }
 
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: 'open' });
     const link = document.createElement('link');
     link.setAttribute('rel', 'stylesheet');
     link.setAttribute('href', '/component/header/header.css');
     shadow.appendChild(link);
 
     const content = document.createElement('div');
-    content.innerHTML = `
-        <header>
-            <div>
-                <h1><slot name="title">歡迎來到美食天堂</slot></h1>
-                <p><slot name="subtitle">品味幸福的每一刻</slot></p>
-            </div>
+    const navType = this.getAttribute('nav-type');
 
-            <restaurant-nav></restaurant-nav>
-        </header>
-    `;
+    if (navType === 'home') {
+      content.innerHTML = `
+          <header>
+              <div>
+                  <h1><slot name="title">歡迎來到美食天堂</slot></h1>
+                  <p><slot name="subtitle">品味幸福的每一刻</slot></p>
+              </div>
+
+              <restaurant-nav-home></restaurant-nav-home>
+          </header>
+      `;
+    } else {
+      content.innerHTML = `
+          <header>
+              <div>
+                  <h1><slot name="title">歡迎來到美食天堂</slot></h1>
+                  <p><slot name="subtitle">品味幸福的每一刻</slot></p>
+              </div>
+
+              <restaurant-nav></restaurant-nav>
+          </header>
+      `;
+    }
 
     shadow.appendChild(content);
   }
